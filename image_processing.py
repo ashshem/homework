73026761abab7c1cf9a07303ae3be4f213d1ed61
Jesse <jr5dr@virginia.edu>
2015-11-30 15:56:09 -0500

    homework for imagine function

---
+++ image_processing.py
@@ -0,0 +1,147 @@
+import pygame as pg
+
+################### PIXEL OPERATIONS ######################
+
+# grayPixel: pixel -> pixel
+# compute and return a gray pixel with the same intensity
+# as the given pixel. A little complication: the intensity
+# values in the 3D numpy array representing the pixels are
+# expressed as 8-bit integers. The range is 0..255. The
+# problem is that if one adds together three of these, to
+# get aonther 8-bit integer, there can be an overflow! So
+# we convert the 8-bit integers to values of type "int" before
+# we do the addition. The "int" type in Python can hold
+# integer values of any size.
+#
+def grayPixel(pixel):
+    red_intensity = int(pixel[0])
+    green_intensity = int(pixel[1])
+    blue_intensity = int(pixel[2])
+    ave_intensity = (red_intensity + green_intensity + blue_intensity)//3
+    
+    return ((ave_intensity, ave_intensity, ave_intensity))
+
+# channel: pixel -> channel -> pixel
+# return a gray pixel with intensity from given channel of given pixel
+def channel(pixel,chan):
+    return (pixel[chan],pixel[chan],pixel[chan])
+
+
+# inverse: pixel -> pixel
+# return the color negative of the given pixel
+def inverse(pixel):
+    return (255-pixel[0], 255-pixel[1], 255-pixel[2])
+
+
+# intensify: pixel -> nat255 -> pixel
+# brighten each channel of pixel by quantity
+#
+# NOTE: there might be an overflow bug in this code!
+# If so, reason through it and fix it. Consider the
+# possible need for a function that brightens an
+# individual pixel intensity, never returning a result
+# greater than 255 or less than 0.
+# Bug: this function needs to be able to be turned off when one pixel
+# in the image reaches either black or white. If pixels that had once
+# been different colors are then changed into either completely white
+# or completely black, rendering them the same, they will never be
+# able to be changed back to their original state by means of this
+# same fucntion
+def intensify(pixel,quantity):
+    px = pixel[0]+quantity
+    if px > 255:
+        px = 255
+    if px < 0:
+        px = 0
+    py = pixel[1]+quantity
+    if py> 255:
+        py = 255
+    if py < 0:
+        py = 0
+    pz = pixel[2]+quantity
+    if pz > 255:
+        pz = 255
+    if pz < 0:
+        pz = 0
+    return (px,py,pz)
+
+# the light and dark functions implement the intensity functions
+# defined above
+
+def lightPixel(pixel):
+    return (intensify(pixel,10))
+
+def darkPixel(pixel):
+    return (intensify(pixel,-10))
+
+
+################### IMAGE OPERATIONS ######################
+
+# invert: modifies image pixel array of image_surf in place
+# replace each pixel with its photographic "negative"
+#
+def invert(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = inverse(pixels3d[x,y])
+
+
+
+# bw: modifies image pixel array of image_surf in place
+# replaces each pixel with a corresponding gray-scale pixel
+def bw(image_surf):
+
+    # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = grayPixel(pixels3d[x,y])
+
+# l: raises the intensity of each pixel in the image by 10 units
+
+def l(image_surf):
+
+     # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = lightPixel(pixels3d[x,y])
+
+# d: darkens the intensity of each pixel in the image by 10 units
+
+def d(image_surf):
+
+     # get pixel dimensions of image
+    rows = image_surf.get_size()[0]
+    cols = image_surf.get_size()[1]
+    
+    # get reference to and lock pixel array
+    pixels3d = pg.surfarray.pixels3d(image_surf)
+
+    # update pixels in place (side effect!)
+    for x in range(rows):
+        for y in range(cols):
+            pixels3d[x,y] = darkPixel(pixels3d[x,y])
+
+    
